10a992bf6c5bbfcdbc8a8688353f1e30b8a64f54
Serenafrisman <49008981@est.ort.edu.ar>
2024-10-29 15:54:08 -0300

    seguir con js

---
+++ js/tercerpiso.js
@@ -0,0 +1,19 @@
+function irAInicio() {
+    window.location.href = "inicio.html"
+}
+function irAInicio() {
+    window.location.href = "inicio.html"
+}
+function irAInicio() {
+    window.location.href = "inicio.html"
+}
+function irAInicio() {
+    window.location.href = "inicio.html"
+}
+
+
+
+document.getElementById("h3plantabaja").addEventListener("click", irAInicio)
+document.getElementById("h3primerpiso").addEventListener("click", irAInicio)
+document.getElementById("h3segundopiso").addEventListener("click", irAInicio)
+document.getElementById("h3tercerpiso").addEventListener("click", irAInicio)
