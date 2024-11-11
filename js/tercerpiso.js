d31c664b6eb97dbd33bf0bce10703d656f3f5d13
Serenafrisman <49008981@est.ort.edu.ar>
2024-11-11 07:46:59 -0300

    comit para seguir

--- js/tercerpiso.js
+++ js/tercerpiso.js
@@ -1,25 +1,18 @@
 
 //como hacer kla funcion de pasar a una pagina 
-
 function irAInicio() {
     window.location.href = "inicio.html"
 }
-function irAInicio() {
-    window.location.href = "inicio.html"
-}
-function irAInicio() {
-    window.location.href = "inicio.html"
-}
-function irAInicio() {
-    window.location.href = "inicio.html"
+function irfavoritos() {
+    window.location.href = "favoritos.html"
 }
 
+document.getElementById('Inicio').addEventListener("click", irAInicio)
+document.getElementById('Favoritos').addEventListener("click", irfavoritos)
 
-
-document.getElementById("h3plantabaja").addEventListener("click", irAInicio)
-document.getElementById("h3primerpiso").addEventListener("click", irAInicio)
-document.getElementById("h3segundopiso").addEventListener("click", irAInicio)
-document.getElementById("h3tercerpiso").addEventListener("click", irAInicio)
+document.getElementById('1er').addEventListener("click", irAInicio)
+document.getElementById('2do').addEventListener("click", irAInicio)
+document.getElementById('3er').addEventListener("click", irAInicio)
 
 //como hacer menu desplegable
 window.onclick = function(event) {
