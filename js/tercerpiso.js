eecbf803b81deef30f52c9f9c5094c8f78e41b4b
Serenafrisman <49008981@est.ort.edu.ar>
2024-11-13 10:34:40 -0300

    hacer inicio y mejorarlo

--- js/tercerpiso.js
+++ js/tercerpiso.js
@@ -1,18 +1,11 @@
 
 //como hacer kla funcion de pasar a una pagina 
 function irAInicio() {
-    window.location.href = "inicio.html"
+    window.location.href = "inicio.html";
 }
 function irfavoritos() {
-    window.location.href = "favoritos.html"
+    window.location.href = "favoritos.html";
 }
-
-document.getElementById('Inicio').addEventListener("click", irAInicio)
-document.getElementById('Favoritos').addEventListener("click", irfavoritos)
-
-document.getElementById('1er').addEventListener("click", irAInicio)
-document.getElementById('2do').addEventListener("click", irAInicio)
-document.getElementById('3er').addEventListener("click", irAInicio)
 
 //como hacer menu desplegable
 function toggleMenu(submenuId) {
@@ -20,13 +13,13 @@ function toggleMenu(submenuId) {
     submenu.style.display = submenu.style.display === "block" ? "none" : "block";
 }
 //pasar del menu desplegablee a  los pisos
+function irAPlantaBaja() {
+    window.location.href = "plantabaja.html";
+}
 function irAPrimeroPiso() {
     window.location.href = "primerpiso.html";
 }
 
-function irAPlantaBaja() {
-    window.location.href = "plantabaja.html";
-}
 
 function irASegundoPiso() {
     window.location.href = "segundopiso.html";
